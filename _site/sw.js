3a25e6bc969327000915f859e6704e34ab9b4bc1
Wh1teXnd <oscar.espejo.blanco@gmail.com>
2022-11-22 17:06:34 +0100

    si

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221122.161139';
+const cacheName = 'chirpy-20221122.170605';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
