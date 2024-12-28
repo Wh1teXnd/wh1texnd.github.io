093f7f555a2312ea2084f2a2d7c5c410e50bb73a
whitexnd <oscar.espejo.blanco@gmail.com>
2024-12-28 13:45:54 +0100

    jekyll update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241228.134109';
+const cacheName = 'chirpy-20241228.134520';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
