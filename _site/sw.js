9a39ebb595eae7657d7eb4b10e1791eed1cca0a5
whitexnd <oscar.espejo.blanco@gmail.com>
2024-12-28 13:57:56 +0100

    index

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241228.134520';
+const cacheName = 'chirpy-20241228.135524';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
